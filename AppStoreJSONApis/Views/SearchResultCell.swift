92cf7707682d1da65ce7980e41db470f8cc5d334
Vasiliy Matveev <thebifid@gmail.com>
2020-06-10 23:04:07 +0700

    Added cells

---
+++ AppStoreJSONApis/Views/SearchResultCell.swift
@@ -0,0 +1,22 @@
+//
+//  SearchResultCell.swift
+//  AppStoreJSONApis
+//
+//  Created by Vasiliy Matveev on 10.06.2020.
+//  Copyright © 2020 Vasiliy Matveev. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .green
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
