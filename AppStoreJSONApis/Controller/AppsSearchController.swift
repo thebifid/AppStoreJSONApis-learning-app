7fa4f2bf06a9b0e868b9b6c9db13a7fbba0d6bf7
Vasiliy Matveev <thebifid@gmail.com>
2020-06-13 20:27:33 +0700

    Added search

--- AppStoreJSONApis/Controller/AppsSearchController.swift
+++ AppStoreJSONApis/Controller/AppsSearchController.swift
@@ -9,9 +9,24 @@
 import UIKit
 import SDWebImage
 
-class AppsSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+class AppsSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
     
     fileprivate let cellId = "id1234"
+    
+    fileprivate let searchController = UISearchController(searchResultsController: nil)
+    
+    
+    fileprivate let enterSearchTermLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Please enter search term above..."
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 20)
+        label.textColor = UIColor(white: 0, alpha: 0.5)
+        
+        return label
+        
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,14 +35,43 @@ class AppsSearchController: UICollectionViewController, UICollectionViewDelegate
         
         collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: cellId)
         
-        fetchItunesApps()
+        collectionView.addSubview(enterSearchTermLabel)
+        enterSearchTermLabel.fillSuperview(padding: .init(top: 100, left: 50, bottom: 0, right: 50))
+        
+        setupSearchBar()
+   //     fetchItunesApps()
     }
     
     fileprivate var appResults = [Result]()
     
     
+    fileprivate func setupSearchBar(){
+        
+        navigationItem.searchController = self.searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        definesPresentationContext = true
+        searchController.searchBar.delegate = self
+    }
+    
+    var timer: Timer?
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+        
+        timer?.invalidate()
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (_) in
+               Service.shared.fetchApps(searchTerm: searchText) { (res, err) in
+                     self.appResults = res
+                     DispatchQueue.main.async {
+                         self.collectionView.reloadData()
+                     }
+                 }
+        })
+    }
+    
     fileprivate func fetchItunesApps(){
-        Service.shared.fetchApps { (results, err) in
+        Service.shared.fetchApps(searchTerm: "twitter") { (results, err) in
             
             if let err = err {
                 print("Failed to fetch apps:", err)
@@ -53,7 +97,7 @@ class AppsSearchController: UICollectionViewController, UICollectionViewDelegate
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SearchResultCell
-        
+        enterSearchTermLabel.isHidden = appResults.count != 0
         cell.appResult = appResults[indexPath.item]
         
         return cell
--- AppStoreJSONApis/Service/Service.swift
+++ AppStoreJSONApis/Service/Service.swift
@@ -12,8 +12,8 @@ class Service {
     
     static let shared = Service() // singleton
     
-    func fetchApps(completion: @escaping ([Result], Error?) -> ()) {
-        let urlString = "https://itunes.apple.com/search?term=instagram&entity=software"
+    func fetchApps(searchTerm: String, completion: @escaping ([Result], Error?) -> ()) {
+        let urlString = "https://itunes.apple.com/search?term=\(searchTerm)&entity=software"
         guard let url = URL(string: urlString) else { return }
         
         //fetch data from internet
